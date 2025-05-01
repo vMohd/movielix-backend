a2c5951a100bbe491d2a7b6c697302f0b44fde4a
vMohd <mohammed.asunbul@gmail.com>
2025-05-01 16:59:51 +0300

    Added CollectionDetail API

--- movielix/urls.py
+++ movielix/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MovieListCreateView, MovieDetailView, TagListCreateView, TagDetailView, CollectionListCreateView
+from .views import MovieListCreateView, MovieDetailView, TagListCreateView, TagDetailView, CollectionListCreateView, CollectionDetailView
 
 urlpatterns = [
     path("movies/", MovieListCreateView.as_view(), name="movie-list-create"),
     path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
     path("collections/", CollectionListCreateView.as_view(), name="collection-list-create"),
+    path("collections/<int:pk>/", CollectionDetailView.as_view(), name="collection-detail"),
     path("tags/", TagListCreateView.as_view(), name="tag-list-create"),
     path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
 ]
--- movielix/views.py
+++ movielix/views.py
@@ -102,3 +102,28 @@ class CollectionListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class CollectionDetailView(APIView):
+    permission_classes = [AllowAny]
+    
+    def get_object(self, pk):
+        return get_object_or_404(Collection, pk=pk)
+    
+    def get(self, request, pk):
+        collection = self.get_object(pk)
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        collection = self.get_object(pk)
+        serializer = CollectionSerializer(collection, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        collection = self.get_object(pk)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
